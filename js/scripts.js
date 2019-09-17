0ff85fee6b0d56165ee5712288002444f79ab169
Emily Baker-King <ebakerking@gmail.com>
2019-09-17 14:04:28 -0500

    added date below time and also added name, linkedin link, and github link

--- js/scripts.js
+++ js/scripts.js
@@ -10,6 +10,10 @@ function showTime() {
     var minute = date.getMinutes(); // 0 - 59
     var seconds = date.getSeconds(); // 0 - 59
     var amPm = "AM";
+    var weekday = date.getDay();
+    var month = date.getMonth();
+    var day = date.getDate();
+    var year = date.getFullYear();
 
     //We need to make sure the hour is properly represeneted in a 12-hr time, and we also need to determine whether we should display AM or PM.
 
@@ -33,11 +37,84 @@ function showTime() {
     minute = (minute < 10) ? "0" + minute : minute;
     seconds = (seconds < 10) ? "0" + seconds : seconds;
 
+    switch(weekday) { 
+        case 0: //If weekday === 0 console.log("Sunday") and then break out of the switch statement
+            weekday = "Sunday";
+            break;
+        case 1:
+            weekday = "Monday"
+            break;
+        case 2:
+            weekday = "Tuesday";
+            break;
+        case 3:
+            weekday = "Wednesday";
+            break;
+        case 4:
+            weekday = "Thursday";
+            break;
+        case 5:
+            weekday = "Friday";
+            break;
+        case 6:
+            weekday = "Saturday";
+    }
+
+    switch(month) {
+        case 0:
+            month = "January";
+            break;
+        case 1:
+            month = "February";
+            break;
+        case 2:
+            month = "March";
+            break;
+        case 3:
+            month = "April";
+            break;
+        case 4:
+            month = "May";
+            break;
+        case 5:
+            month = "June";
+            break;
+        case 6:
+            month = "July";
+            break;
+        case 7:
+            month = "August";
+            break;
+        case 8:
+            month = "September";
+            break;
+        case 9:
+            month = "October";
+            break;
+        case 10:
+            month = "November";
+            break;
+        case 11:
+            month = "December"
+    }
+
     var time = hour + ":" + minute + ":" + seconds + " " + amPm;
 
+    var date = weekday + ", " + month + " " + day + ", " + year;
+
     document.getElementById("clockDisplay").innerText = time;
+
+    document.getElementById("dateDisplay").innerText = date;
 
     setTimeout(showTime, 1000); //Schedule the showTime function to be called after 1000 ms (1 second)
 }
 
 showTime();
+
+
+/*
+    JavaScript Clock Challenges:
+        1. Add a different font (of your choice) for your clock to use.
+        2. Display the current date below the time. For example: Today's date is: Tuesday, September 17th. *Hint - hours, minutes, and seconds aren't the only thing we can pull from a date object*
+        3. Add a header to your clock displaying your name, GitHub link, and LinkedIn profile link.
+*/
